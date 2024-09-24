8eec7dfd4b9e87ea81b06ffd24dca502e3d2d2b2
Nicole Dragassakis <niki_dragasakis@icloud.com>
2024-09-23 21:15:27 -0400

    Create recursive function to calculate department salary - TASK TWO

--- department_salary.js
+++ department_salary.js
@@ -55,3 +55,27 @@ const company = {
     ]
 };
 console.log(company);
+
+//TASK TWO
+
+console.log("TASK TWO"); //added for better console readability
+
+function calculateDepartmentSalary(department) { //this function will calculate the total salary for each department
+    let totalSalary = 0;
+
+    for (let employee of department.employees) {
+        totalSalary += employee.salary; //
+
+        if (employee.subordinates > 0) {
+            totalSalary += calculateDepartmentSalary({ employees: employee.subordinates });
+        }
+    }
+
+    return totalSalary;
+}
+
+const totalEngDepartmentSalary = calculateDepartmentSalary(company.departments[0]);
+console.log(`Total Salary for Engineering department: $${totalEngDepartmentSalary}`);
+
+const totalSaleDepartmentSalary = calculateDepartmentSalary(company.departments[1]);
+console.log(`Total Salary for Sales department: $${totalSaleDepartmentSalary}`);
